baa6a21f8fe7c203a33cd7b16c32cce0f76de2fb
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2024-11-16 21:01:24 +0530

    Resolve flickering using double buffering

--- windows/level1/level1.cpp
+++ windows/level1/level1.cpp
@@ -1,55 +1,63 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cstring>
 #include <conio.h>
 #include <tuple>
+#include <windows.h>
 
-const int GRID_SIZE = 10;
+const int GRID_SIZE = 25;
 
 void initializeGrid(char grid[GRID_SIZE][GRID_SIZE], int &playerX, int &playerY);
+void updateGrid(char prevGrid[GRID_SIZE][GRID_SIZE], char newGrid[GRID_SIZE][GRID_SIZE]);
 void displayGrid(char grid[GRID_SIZE][GRID_SIZE]);
-void clearScreen();
-void moveCursorToTop();
+void clearConsole();
+void getCursorPosition();
+void setCursorAppearance(bool visible);
 void placeRandomTarget(char[GRID_SIZE][GRID_SIZE], int &playerX, int &playerY, int &targetX, int &targetY);
 bool movePlayer(char grid[GRID_SIZE][GRID_SIZE], int &playerX, int &playerY, char direction);
 
 int main()
 {
-    char grid[GRID_SIZE][GRID_SIZE];
+    char prevGrid[GRID_SIZE][GRID_SIZE];
+    char newGrid[GRID_SIZE][GRID_SIZE];
 
     int playerX = GRID_SIZE / 2;
     int playerY = GRID_SIZE / 2;
 
     int targetX, targetY;
 
-    initializeGrid(grid, playerX, playerY);
+    initializeGrid(prevGrid, playerX, playerY);
+    initializeGrid(newGrid, playerX, playerY);
 
-    placeRandomTarget(grid, playerX, playerY, targetX, targetY);
+    placeRandomTarget(newGrid, playerX, playerY, targetX, targetY);
 
     int targetsReached = 0;
     int targetsToReach = 4;
 
+    clearConsole();
+    // getCursorPosition();
+    setCursorAppearance(false);
+    displayGrid(newGrid);
+
     while (targetsReached < targetsToReach)
     {
-        clearScreen();
-        // moveCursorToTop();
-        displayGrid(grid);
-
         char input = _getch();
 
-        if (movePlayer(grid, playerX, playerY, input))
+        if (movePlayer(newGrid, playerX, playerY, input))
         {
             if (playerX == targetX && playerY == targetY)
             {
                 targetsReached++;
-                placeRandomTarget(grid, playerX, playerY, targetX, targetY);
+                placeRandomTarget(newGrid, playerX, playerY, targetX, targetY);
             }
         }
+
+        updateGrid(prevGrid, newGrid);
     }
-    grid[targetY][targetX] = '.';
-    // moveCursorToTop();
-    clearScreen();
-    displayGrid(grid);
+
+    newGrid[targetY][targetX] = '.';
+    updateGrid(prevGrid, newGrid);
     std::cout << "Level Complete!";
     return 0;
 }
@@ -60,26 +68,26 @@ void displayGrid(char grid[GRID_SIZE][GRID_SIZE])
     {
         for (int j = 0; j < GRID_SIZE; j++)
         {
-            std::cout << grid[i][j] << ' ';
+            // std::cout << grid[i][j] << ' ';
+            std::cout << grid[i][j];
         }
         std::cout << '\n';
     }
 }
 
-void clearScreen()
+void clearConsole()
 {
-    std::system("CLS");
-}
-
-void moveCursorToTop()
-{
-    // std::cout << "\033H";
-    printf("\033[0;0H\033[2J");
+    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD topLeft = {0, 0};
+    std::cout.flush();
+    SetConsoleCursorPosition(hOut, topLeft);
 }
 
 bool movePlayer(char grid[GRID_SIZE][GRID_SIZE], int &playerX, int &playerY, char direction)
 {
-    grid[playerY][playerX] = '.';
+    bool move = true;
+    int originalX = playerX;
+    int originalY = playerY;
     switch (direction)
     {
     case 'h':
@@ -107,10 +115,15 @@ bool movePlayer(char grid[GRID_SIZE][GRID_SIZE], int &playerX, int &playerY, cha
         }
         break;
     default:
-        return false;
+        move = false;
     }
-    grid[playerY][playerX] = 'P';
-    return true;
+    if (move)
+    {
+        grid[originalY][originalX] = '.';
+        grid[playerY][playerX] = 'P';
+        return true;
+    }
+    return false;
 }
 
 void initializeGrid(char grid[GRID_SIZE][GRID_SIZE], int &playerX, int &playerY)
@@ -139,4 +152,55 @@ void placeRandomTarget(char grid[GRID_SIZE][GRID_SIZE], int &playerX, int &playe
         targetY = std::rand() % GRID_SIZE;
     }
     grid[targetY][targetX] = 'X';
+}
+
+void setCursorAppearance(bool visible)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+
+    GetConsoleCursorInfo(hConsole, &cursorInfo);
+
+    cursorInfo.bVisible = visible;
+
+    SetConsoleCursorInfo(hConsole, &cursorInfo);
+}
+
+void setCursorPosition(int x, int y)
+{
+    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout.flush();
+    COORD coord = {(SHORT)x, (SHORT)y};
+    SetConsoleCursorPosition(hOut, coord);
+}
+
+void getCursorPosition()
+{
+    int x, y;
+    static const HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(hConsole, &csbi))
+    {
+        x = csbi.dwCursorPosition.X;
+        y = csbi.dwCursorPosition.Y;
+    }
+    std::cout << "Current cursor position: (" << x << ", " << y << ")\n";
+}
+
+void updateGrid(char prevGrid[GRID_SIZE][GRID_SIZE], char newGrid[GRID_SIZE][GRID_SIZE])
+{
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            if (newGrid[i][j] == prevGrid[i][j])
+            {
+                continue;
+            }
+            setCursorPosition(j, i);
+            std::cout << newGrid[i][j];
+        }
+    }
+    std::cout.flush();
+    std::memcpy((char *)prevGrid, (char const *)newGrid, GRID_SIZE * GRID_SIZE);
 }
